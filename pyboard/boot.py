66bb2a2f9cb13cb85f14ef4160131a5ff0d867b1
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-23 08:51:20 +0200

    renamed emptymain to copymodemain

--- pyboard/boot.py
+++ pyboard/boot.py
@@ -5,7 +5,7 @@ import uos
 if path.exists('/flash/bootincopymode'):
 	uos.remove('/flash/bootincopymode')	
 	pyb.usb_mode('VCP+MSC')
-	pyb.main('emptymain.py')
+	pyb.main('copymodemain.py')
 else:
 	pyb.usb_mode('VCP')
 	pyb.main('main.py')

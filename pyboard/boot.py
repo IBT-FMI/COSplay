49a31eacf93ee02b82194fe1129eb899aa791971
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-24 10:02:12 +0200

    switch laser off immediately

--- pyboard/boot.py
+++ pyboard/boot.py
@@ -2,6 +2,10 @@ import path
 import pyb
 import uos
 
+#switch of laser immediately
+pin_out = pyb.Pin('Y1',pyb.Pin.OUT_PP,pull=pyb.Pin.PULL_UP)
+pin_out.value(1)
+
 if path.exists('/flash/bootincopymode'):
 	uos.remove('/flash/bootincopymode')	
 	pyb.usb_mode('VCP+MSC')
--- pyboard/copymodemain.py
+++ pyboard/copymodemain.py
@@ -1,1 +1,5 @@
+#switch laser off immediately
+pin_out = pyb.Pin('Y1',pyb.Pin.OUT_PP,pull=pyb.Pin.PULL_UP)
+pin_out.value(1)
+
 pyb.LED(1).on()

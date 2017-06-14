fde24539a8ca3a0ec136012c245c750f5b79982c
Aymanns Florian <faymanns@student.ethz.ch>
2017-06-14 10:28:53 +0200

    updated docstrings

--- pyboard/config.py
+++ pyboard/config.py
@@ -1,14 +1,12 @@
 """Path to folder with sequences.
 Should only be changed if sequences are manually copied to a different path.
-Default is '/sd/sequence_library'.
 """
-library_path = '/flash/sequence_library'
+library_path = ''
 
-"""Timing accuracy can be 'us' or 'ms'. 'us' mode only works for sequences
-with a total duration of less than ~19min47s
-"""
+"""Timing accuracy can be 'us' or 'ms'."""
 accuracy = 'us'
 
+"""Values of pins when stimulus is active"""
 on_value_out_channel1 = 0
 on_value_out_channel2 = 0
 on_value_out_channel3 = 0

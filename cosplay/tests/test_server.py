19ae700944933137a6e615fa0504f14f75317055
Aymanns Florian <faymanns@student.ethz.ch>
2017-07-10 14:01:50 +0200

    redirect output of socat

--- cosplay/tests/test_server.py
+++ cosplay/tests/test_server.py
@@ -2,13 +2,14 @@ from cosplay.pkt import Packet
 import cosplay.server
 from cosplay.serial_port import SerialPort
 import time
+import sys
 
-def test_server():
+def test_server(port):
 	sp = SerialPort()
 	connected=False
 	i=0
 	while not connected and i<1000:
-		connected = sp.connect_serial('/dev/pts/3')
+		connected = sp.connect_serial(port)
 		i += 1
 		time.sleep(0.1)
 	if connected:
@@ -19,4 +20,4 @@ def test_server():
 	assert pkt.receive(time_out=10000)==pkt.ANS_no
 
 if __name__=="__main__":
-	test_server()
+	test_server(sys.argv[1])

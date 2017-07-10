26c6190b7dc073365ce0238eb0be61c49ca55f23
Aymanns Florian <faymanns@student.ethz.ch>
2017-07-10 10:05:30 +0200

    added time out

--- cosplay/tests/test_server.py
+++ cosplay/tests/test_server.py
@@ -1,14 +1,22 @@
 from cosplay.pkt import Packet
 import cosplay.server
 from cosplay.serial_port import SerialPort
+import time
 
 def test_server():
 	sp = SerialPort()
-	sp.connect_serial('/dev/pts/3')
+	connected=False
+	i=0
+	while not connected and i<1000:
+		connected = sp.connect_serial('/dev/pts/3')
+		i += 1
+		time.sleep(0.1)
+	if connected:
+		print('Connected to /dev/pts/3')
 	pkt = Packet(sp)
 	pkt.send("Test string!")
 	pkt.send(pkt.INS_check_for_sequences_on_server)
-	assert pkt.receive()==pkt.ANS_no
+	assert pkt.receive(time_out=10000)==pkt.ANS_no
 
 if __name__=="__main__":
 	test_server()

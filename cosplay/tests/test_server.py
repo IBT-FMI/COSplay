65bafaf131788f9b62e4bba7beac25095fe81aa6
Aymanns Florian <faymanns@student.ethz.ch>
2017-07-11 15:24:13 +0200

    test pkt class

--- cosplay/tests/test_server.py
+++ cosplay/tests/test_server.py
@@ -4,6 +4,34 @@ from cosplay.serial_port import SerialPort
 import time
 import sys
 
+def test_pkt(port1, port2):
+	print('Port 1: ',port1)
+	sp1 = SerialPort()
+	connected=False
+	i=0
+	while not connected and i<1000:
+		connected = sp1.connect_serial(port1)
+		i += 1
+		time.sleep(0.1)
+	if connected:
+		print('Connected to {0}'.format(port1))
+	pkt1 = Packet(sp1)
+	print('Port 2: ',port2)
+	sp2 = SerialPort()
+	connected=False
+	i=0
+	while not connected and i<1000:
+		connected = sp2.connect_serial(port2)
+		i += 1
+		time.sleep(0.1)
+	if connected:
+		print('Connected to {0}'.format(port2))
+	pkt2 = Packet(sp2)
+
+	pkt1.send("Test string!")
+	s = pkt2.receive(time_out=100000)
+	print(s)
+
 def test_server(port):
 	print('Port: ',port)
 	sp = SerialPort()
@@ -14,13 +42,14 @@ def test_server(port):
 		i += 1
 		time.sleep(0.1)
 	if connected:
-		print('Connected to /dev/pts/3')
+		print('Connected to {0}'.format(port))
 	pkt = Packet(sp)
 	pkt.send("Test string!")
 	pkt.send(pkt.INS_check_for_sequences_on_server)
-	answer = pkt.receive(time_out=10000)
+	answer = pkt.receive(time_out=100000)
 	print('Answer: ',answer)
 	assert answer==pkt.ANS_no
 
 if __name__=="__main__":
-	test_server(sys.argv[1])
+	#test_server(sys.argv[1])
+	test_pkt(sys.argv[1],sys.argv[2])

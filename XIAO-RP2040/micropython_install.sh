53a1466b3897fa46607910ccc27d31f8747f17c8
Horea Christian <chr@chymera.eu>
2023-06-01 10:10:51 -0400

    Removed deprecated install file

--- XIAO-RP2040/micropython_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This will need to be improved based on:
-# https://github.com/orgs/micropython/discussions/11366
-
-if ! wget "https://micropython.org/resources/firmware/seeed_xiao_nrf52-20230426-v1.20.0.uf2" ; then
-	echo "The hard-coded download link is outdated. This is to be expected."
-	echo "Please find the current download link under `https://micropython.org/download/seeed_xiao_nrf52/`, and download the file to this directory."
-fi

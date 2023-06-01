37bb35a94cb1c4e25e4782a7d4956ee309344c40
Horea Christian <chr@chymera.eu>
2023-06-01 15:58:40 -0400

    Fixed install script

--- devices/XIAO-RP2040/install_script.sh
+++ devices/XIAO-RP2040/install_script.sh
@@ -13,8 +13,13 @@ if [ ! -d "$myboard" ]; then
 fi
 
 pushd ..
-	./mpdl.py seeed_xiao_nrf5
+	./mpdl.py seeed_xiao_nrf52
 popd
 	umount ${myboard}
 	sleep 2
+	while ! $(mpremote fs ls &> /dev/null); do
+		echo "The device has not auto-connected, please press the “RESET”button on the device once to continue."
+		sleep 5
+	done
 	mpremote fs cp device_files/* :
+	mpremote reset
